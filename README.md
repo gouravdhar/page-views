b783ca98cda74826e48e3ed7b8078721ec715465
batman <dharg57@gmail.com>
2022-02-01 22:53:11 +0530

    profile page

---
+++ README.md
@@ -0,0 +1,1 @@
+# www.gourav-dhar.com
---
+++ precache-manifest.9ea9ed516e1a0e8785a6aa2c9aa5af2a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7af53f985b7c6925a2bd4e6ae7ceed7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "692350bd3c279d57f550",
+    "url": "/static/css/2.74662cff.chunk.css"
+  },
+  {
+    "revision": "d8863d834c3bc8d26283",
+    "url": "/static/css/main.b166e648.chunk.css"
+  },
+  {
+    "revision": "692350bd3c279d57f550",
+    "url": "/static/js/2.2aeb7b0a.chunk.js"
+  },
+  {
+    "revision": "4d929bc7210a639efed2",
+    "url": "/static/js/3.29435903.chunk.js"
+  },
+  {
+    "revision": "d8863d834c3bc8d26283",
+    "url": "/static/js/main.daf300f4.chunk.js"
+  },
+  {
+    "revision": "87478e93697db7cd51a3",
+    "url": "/static/js/runtime~main.7a15c720.js"
+  },
+  {
+    "revision": "8fb0e5fd721a02afda190255b5ee649d",
+    "url": "/static/media/about.8fb0e5fd.png"
+  },
+  {
+    "revision": "99dc981ccfda2d09e589f75a5c885a58",
+    "url": "/static/media/avatar.99dc981c.jpeg"
+  },
+  {
+    "revision": "bc2c7afa280d7a7d54e2c5ecf515e8a6",
+    "url": "/static/media/home-bg.bc2c7afa.jpg"
+  },
+  {
+    "revision": "1aed08b3e1b66991d921d25dbdcf28ed",
+    "url": "/static/media/home-main.1aed08b3.svg"
+  },
+  {
+    "revision": "977e76b178f6edd402c0c1d50faaa9a2",
+    "url": "/static/media/logo.977e76b1.png"
+  },
+  {
+    "revision": "cf8c6935f68fde1543684d4a817f213a",
+    "url": "/static/media/pre.cf8c6935.svg"
+  },
+  {
+    "revision": "cadd2768eef665a1f1a737c07fd0a9c1",
+    "url": "/static/media/rithiksamanthularesume.cadd2768.pdf"
+  }
+]);
